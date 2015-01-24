e9c477c9504dea1dd12a7b6bf2c869382533f63d
Maarc <marc@zottner.com>
2015-01-24 18:49:09 +0100

    CleanMyMac2 added

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install CleanMyMac2 to /Applications.
+#
+# include cleanmymac
+#
+class cleanmymac(){
+  package { 'CleanMyMac2':
+    source => "http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg",
+    provider => 'appdmg'
+  }
 }
