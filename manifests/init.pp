4d45288a38bea5943bead4f6548494076d7bd77e
Marc Zottner <marc@zottner.com>
2015-01-24 20:16:09 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,19 +6,20 @@ class mymissingmodules::cleanmymac {
     }
 }
 
-# include mymissingmodules::flashplayer
-class mymissingmodules::flashplayer {
-    $install_file = '/Applications/Install\ Adobe\ Flash\ Player.app'
-    package { 'AdobeFlashPlayerInstaller':
-      source => "http://aihdownload.adobe.com/bin/live/AdobeFlashPlayerInstaller_16_ltrosxd_aaa_aih.dmg",
-      provider => 'appdmg'
-    }
-    ~> exec { 'Install Flash Player':
-       command => "/usr/bin/open ${install_file}",
-       logoutput => 'on_failure',
-       refreshonly => true,
-    }
-    -> file { $install_file:
-       ensure => 'absent',
-  }
-}
+# Does not work currently as "Install Adobe Flash Player.app" is not able to execute and finish the setup
+## include mymissingmodules::flashplayer
+#class mymissingmodules::flashplayer {
+#    $install_file = '/Applications/Install\ Adobe\ Flash\ Player.app'
+#    package { 'AdobeFlashPlayerInstaller':
+#      source => "http://aihdownload.adobe.com/bin/live/AdobeFlashPlayerInstaller_16_ltrosxd_aaa_aih.dmg",
+#      provider => 'appdmg'
+#    }
+#    ~> exec { 'Install Flash Player':
+#       command => "/usr/bin/open ${install_file}",
+#       logoutput => 'on_failure',
+#       refreshonly => true,
+#    }
+#    -> file { $install_file:
+#       ensure => 'absent',
+#  }
+#}
