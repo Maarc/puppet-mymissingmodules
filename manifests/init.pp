c308a1984db671939cfb77443664f3994fcfa9bb
Marc Zottner <marc@zottner.com>
2015-01-24 19:18:10 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,8 @@
 # Public: Install CleanMyMac2 to /Applications.
-#
-# include cleanmymac
-#
-class cleanmymac (
-  $version = '2',
-  ) {
-    package { 'CleanMyMac${version}':
-      source => "http://dl.devmate.com/com.macpaw.CleanMyMac${version}/CleanMyMac${version}.dmg",
+
+class mymissingmodules::cleanmymac {
+    package { 'CleanMyMac2':
+      source => "http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg",
       provider => 'appdmg'
     }
 }
