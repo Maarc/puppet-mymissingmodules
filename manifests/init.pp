109e862c73d97661898c5faad26433b2488c7449
Marc Zottner <marc@zottner.com>
2015-01-24 19:44:12 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,4 @@
-# Public: Install CleanMyMac2 to /Applications.
-
+# include mymissingmodules::cleanmymac
 class mymissingmodules::cleanmymac {
     package { 'CleanMyMac2':
       source => "http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg",
@@ -7,11 +6,19 @@ class mymissingmodules::cleanmymac {
     }
 }
 
+# include mymissingmodules::flashplayer
 class mymissingmodules::flashplayer {
+    $install_file = '/Applications/Install\ Adobe\ Flash\ Player.app'
     package { 'AdobeFlashPlayerInstaller':
       source => "http://aihdownload.adobe.com/bin/live/AdobeFlashPlayerInstaller_16_ltrosxd_aaa_aih.dmg",
       provider => 'appdmg'
     }
+    ~> exec { 'Install Flash Player':
+       command => "/usr/bin/open ${install_file}",
+       logoutput => 'on_failure',
+       refreshonly => true,
+    }
+    -> file { $install_file:
+       ensure => 'absent',
+  }
 }
-
-
